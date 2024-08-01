528927fef1ed19625178a5f032732552b59c6a0a
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-08-01 10:22:51 -0700

    fixed code

--- firebase.js
+++ firebase.js
@@ -1,13 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-import {getFirestore} from "firebase/firestore"
-
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAvRjgaBCtNwgT3kgzk_vtqdKdc0ixIXuY",
   authDomain: "inventory-management-e4401.firebaseapp.com",
@@ -20,7 +16,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
-export {firestore}
+// Initialize Analytics only on the client side
+if (typeof window !== "undefined") {
+  isSupported().then((yes) => {
+    if (yes) {
+      const analytics = getAnalytics(app);
+    }
+  }).catch((error) => {
+    console.log("Analytics is not supported in this environment:", error);
+  });
+}
+
+export { firestore };
